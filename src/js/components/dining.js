34b813267ffb8196e0a118ca4de63075168a05cb
RLTB-Rommel <rommelaunario101@gmail.com>
2025-06-12 08:31:29 +0800

    fixed dining image path

--- src/js/components/dining.js
+++ src/js/components/dining.js
@@ -2,7 +2,7 @@ const diningData = await fetch("../data/dining.json").then(res => res.json());
 
 export function renderDining(index = 0) {
   const data = diningData[index];
-  document.getElementById("dining-img").style.backgroundImage = `url('${data.image}')`;
+  document.getElementById("dining-img").src = restaurant.image;
   document.getElementById("dining-name").textContent = data.name;
   document.getElementById("dining-stars").textContent = "⭐".repeat(Math.floor(data.stars)) + (data.stars % 1 >= 0.5 ? "☆" : "");
   document.getElementById("dining-desc").textContent = data.description;
