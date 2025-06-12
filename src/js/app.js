1616139064a1faa5d939253305a1eb549d698b2b
RLTB-Rommel <rommelaunario101@gmail.com>
2025-06-12 11:02:54 +0800

    updated cards

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,18 @@
 import { renderDining, setupDiningRadio } from './components/dining.js';
+import { renderAttractions, setupAttractionsRadio } from './components/attractions.js';
+import { renderAdventure, setupAdventureRadio } from './components/adventure.js';
+import { renderEducational, setupEducationalRadio } from './components/educational.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   renderDining(0);
   setupDiningRadio();
+
+  renderAttractions(0);
+  setupAttractionsRadio();
+
+  renderAdventure(0);
+  setupAdventureRadio();
+
+  renderEducational(0);
+  setupEducationalRadio();
 });
